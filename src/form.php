fa52ec3dd2408bf5bf3f8995e850dc26205101e0
Gustavo_Nunes <gustavo.r.sousa@icloud.com>
2026-05-19 17:32:30 -0300

    Update form.php

--- src/form.php
+++ src/form.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de Login</title> <link rel="stylesheet" href="form.css?v=2">
+    <title>Tela de Login</title>
+    <link rel="stylesheet" href="form.css?v=99">
 </head>
 <body>
     <img src="img_adega.jpeg">
-<<<<<<< HEAD
-    <h1>Adega UniNine</h1><br>
+    <h1>Adega UniNine</h1>
     <form method="post" action="relatorio.php">
-    <h4>Login</h4>
-    <input type="text" id="login" name="login" placeholder="Nome:" required><br><br>
-    <input type="password" id="senha" name="senha" placeholder="Senha:" required><br><br>
-    <div class="entrar"><input type="submit" value="Entrar"></div><br><br>
-    <div class="cadastro-link">
-    <a href="cadastro.php">Cadastrar Funcionário</a>
-</div>
-</form>
+        <h4>Login</h4>
+        <input type="text" id="login" name="login" placeholder="Nome:" required><br><br>
+        <input type="password" id="senha" name="senha" placeholder="Senha:" required><br><br>
+        <div class="entrar"><input type="submit" value="Entrar"></div><br><br>
+        <div class="cadastro-link">
+            <a href="cadastro.php">Cadastrar Funcionário</a>
+        </div>
+    </form>
 </body>
 </html>
